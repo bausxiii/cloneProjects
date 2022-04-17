304e77197baf4bf49c8bb16b3cb6c88a26f78dfc
bausxiii <65490597+bausxiii@users.noreply.github.com>
2022-04-17 17:31:24 +0300

    Create digitalstream.py

---
+++ digitalstream.py
@@ -0,0 +1,56 @@
+"""Digital Stream
+A screensaver in the style of The Matrix movie's visuals.
+
+Tags: tiny, artistic, beginner, scrolling"""
+
+import random
+import shutil
+import sys
+import time
+
+# Set up the constants:
+MIN_STREAM_LENGTH = 5           # Changing to 1 or 50
+MAX_STREAM_LENGTH = 13          # Changing this to 100
+PAUSE = 0.1                     # Changing this to 0.0 or 2.0
+STREAM_CHARS = ['0', '1']
+
+# Density can range from 0.0 to 1.0:
+DENSITY = 0.02  # Changing this to 0.10 or 0.30
+
+# Get the size of the terminal window:
+WIDTH = shutil.get_terminal_size()[0]
+# We cant print to the last column on Windows without it adding
+# a newline automatically, so reduce the width by one:
+WIDTH -= 1
+
+print('Digital Stream')
+print('Press Ctrl-C to quit')
+time.sleep(1)
+
+try:
+    # For each column, when the counter is 0, no stream is shown.
+    # Otherwise, it acts as a counter for how many times a 1 or 0
+    # should be displayed in that column.
+    columns = [0] * WIDTH
+
+    while True:
+        # Set the counter for each column:
+        for i in range(WIDTH):
+            if columns[i] == 0:
+                if random.random() <= DENSITY:
+                    # Restart a stream on this column.
+                    columns[i] = random.randint(MIN_STREAM_LENGTH,
+                                                MAX_STREAM_LENGTH)
+
+            # Display an empty space or a 1/0 character.
+            if columns[i] > 0:
+                print(random.choice(STREAM_CHARS), end='')
+                columns[i] -= 1
+            else:
+                print(' ', end='')
+
+        print()  # Print a newline at the end of the row of columns.
+        sys.stdout.flush()  # Make sure text appears on the screen.
+        time.sleep(PAUSE)
+except KeyboardInterrupt:
+    sys.exit()
