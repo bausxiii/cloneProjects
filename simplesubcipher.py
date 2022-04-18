a0c12769dc1622bc35849d9cc6be7a1832cbc996
bausxiii <65490597+bausxiii@users.noreply.github.com>
2022-04-18 16:11:09 +0300

    Create simplesubcipher.py
    missing translateMessage func will be add.

---
+++ simplesubcipher.py
@@ -0,0 +1,120 @@
+"""Simple Substitution Cipher,
+A simple substitution cipher has a one-to-one translation for each
+symbol in the plaintext and each symbol in the ciphertext.
+More info at: https://en.wikipedia.org/wiki/Substitution_cipher
+
+Tags: short, cryptography, math"""
+
+import random
+import sys
+
+try:
+    import pyperclip  # pyperclip copies text to the clipboard.
+except ImportError:
+    pass
+
+LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+
+def main():
+    print('''Simple Substitution Cipher,
+A simple substitution cipher has a one-to-one translation for each
+symbol in the plaintext and each symbol in the ciphertext.''')
+
+    # Let the user specify if they are encrypting or decrypting:
+    while True:  # Keepasking until the user enters e or d.
+        print('Do you want to (e)ncrypt or (d)ecrypt?')
+        response = input('> ').lower()
+
+        if response.startswith('e'):
+            myMode = 'encrypt'
+            break
+        elif response.startswith('d'):
+            myMode = 'decrypt'
+            break
+
+        print('Please enter the letter e or d.')
+
+    # Let the user specify the key to use:
+    while True:  # Keep asking until the user enters a valid key.
+        print(f'Please specify the key to use. ({len(LETTERS)} chars long)')
+
+        if myMode == 'encrypt':
+            print('Or enter RANDOM to have one generated for you.')
+
+        response = input('> ').upper()
+
+        if response == 'RANDOM' and not myMode == 'decrypt':
+            myKey = generateRandomKey()
+            print(f'The key is {myKey}. KEEP THIS SECRET!')
+            break
+        else:
+            if checkKey(response):
+                myKey = response
+                break
+
+    # Let the user specify the message to encrypt/decrypt:
+    print(f'Enter the message to {myMode}.')
+    myMessage = input('> ')
+
+    # Perform the encryption/decryption:
+    if myMode == 'encrypt':
+        translated = encryptMessage(myMessage, myKey)
+    elif myMode == 'decrypt':
+        translated = decryptMessage(myMessage, myKey)
+
+    # Display the results:
+    print('The %sed message is:' % (myMode))
+    print(translated)
+
+    try:
+        pyperclip.copy(translated)
+        print('Full %sed text copied to clipboard.' % (myMode))
+    except:
+        pass  # Do nothing if pyperclip wasnt installed.
+
+
+def checkKey(key):
+    """Return True if key is valid. Otherwise return False."""
+    keyList = list(key)
+    lettersList = list(LETTERS)
+
+    keyList.sort()
+    lettersList.sort()
+
+    if keyList != lettersList:
+        print('There is an error in the key or symbol set.')
+        return False
+    return True
+
+
+def encryptMessage(message, key):
+    """Encryp the message using the key."""
+    return translateMessage(message, key, 'encrypt')
+
+
+def decryptMessage(message, key):
+    """Decrypt the message using the key."""
+    return translateMessage(message, key, 'decrypt')
+
+
+### Continue...
+def translateMessage(message, key, mode):
+    """Encrypt or decrypt the message using the key."""
+    translated = ''
+    pass
+
+
+def generateRandomKey():
+    """Generate and return a random encryption key."""
+    key = list(LETTERS)  # Get a list from the LETTERS string.
+    random.shuffle(key)  # Randomly shuffle the list.
+    return ''.join(key)  # Get a string from the list.
+
+
+# If this program was run (instead of imported), run the program:
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit()
